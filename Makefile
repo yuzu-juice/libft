9f561c2e9bb1c28e0ca32c033a747057d8ca1bd9
yuzu-juice <yuzujuicesec@gmail.com>
2025-10-21 11:52:29 +0900

    putendl

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: takitaga <takitaga@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/05/04 11:11:26 by takitaga          #+#    #+#              #
-#    Updated: 2025/10/21 11:48:39 by takitaga         ###   ########.fr        #
+#    Updated: 2025/10/21 11:50:36 by takitaga         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -43,7 +43,7 @@ SRCS	= ft_isalpha.c \
 		ft_striteri.c \
 		ft_putchar_fd.c \
 		ft_putstr_fd.c \
-		# ft_putendl_fd.c \
+		ft_putendl_fd.c \
 		# ft_putnbr_fd.c  \
 		# ft_putchar.c \
 		# ft_putstr.c \
--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: takitaga  <takitaga@student.42tokyo.>      +#+  +:+       +#+        */
+/*   By: takitaga <takitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 15:39:55 by takitaga          #+#    #+#             */
-/*   Updated: 2024/05/19 13:50:43 by takitaga         ###   ########.fr       */
+/*   Updated: 2025/10/21 11:52:05 by takitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,3 @@ void	ft_putendl_fd(char *s, int fd)
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
 }
-
-// #include <stdio.h>
-// #include <unistd.h>
-// #include <fcntl.h>
-// #include <string.h>
-
-// int main() {
-//     char filename[] = "testfile.txt";
-//     char test_str[] = "ABC";
-//     char *buffer;
-
-// 	buffer = malloc(sizeof(char) * (ft_strlen(test_str) + 1));
-//     int fd = open(filename, O_CREAT | O_RDWR, 0666);
-//     if (fd == -1) {
-//         perror("open");
-//         return 1;
-//     }
-
-//     ft_putendl_fd(test_str, fd);
-
-//     // ファイルの内容を確認するために読み取る
-//     lseek(fd, 0, SEEK_SET); // ファイルポインタを先頭に移動
-//     read(fd, buffer, ft_strlen(test_str) + 1);
-//     buffer[ft_strlen(test_str) + 1] = '\0'; // 終端文字を追加
-
-//     // 結果を表示
-//     printf("Written string: %s\n", buffer);
-
-//     close(fd);
-//     remove(filename);
-
-//     return 0;
-// }
