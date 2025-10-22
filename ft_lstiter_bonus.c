85040d416bc2c7c529257d17aea809cfe7b4b980
yuzu-juice <yuzujuicesec@gmail.com>
2025-10-22 15:07:52 +0900

    lstiter

---
+++ ft_lstiter_bonus.c
@@ -0,0 +1,46 @@
+#include "libft_bonus.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*tmp;
+
+	if (lst == NULL)
+		return ;
+	tmp = lst;
+	while (tmp)
+	{
+		f(tmp->content);
+		tmp = tmp->next;
+	}
+}
+
+// #include <stdio.h>
+// #include <string.h>
+// void replace_first_char_to_a(void *ptr)
+// {
+// 	char *str = ptr;
+// 	str[0] = 'a';
+// }
+
+// int main()
+// {
+// 	t_list *first = ft_calloc(1, sizeof(t_list));
+// 	first->content = strdup("first");
+// 	first->next = NULL;
+
+// 	t_list *new = ft_calloc(1, sizeof(t_list));
+// 	new->content = strdup("second");
+// 	new->next = NULL;
+// 	ft_lstadd_back(&first, new);
+
+// 	printf("Before apply:\n");
+// 	printf("%s, %p\n", (char *)first->content, first->next);
+// 	printf("%s, %p\n", (char *)first->next->content, first->next->next);
+
+// 	ft_lstiter(first, replace_first_char_to_a);
+// 	printf("After apply:\n");
+// 	printf("%s, %p\n", (char *)first->content, first->next);
+// 	printf("%s, %p\n", (char *)first->next->content, first->next->next);
+
+// 	return 0;
+// }
