939963358753386d99bbc7810a8cfb7549a5161a
yuzu-juice <yuzujuicesec@gmail.com>
2025-10-30 15:25:01 +0900

    update

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -17,7 +17,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	t_list	*current;
 	t_list	*next;
 
-	if (lst == NULL || *lst == NULL)
+	if (lst == NULL || *lst == NULL || del == NULL)
 		return ;
 	current = *lst;
 	while (current)
